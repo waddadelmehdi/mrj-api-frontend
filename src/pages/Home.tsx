d0c86bf3add1921c87845c28520236347c221a86
WADDAD EL MEHDI <elmehdi.waddad@emsi-edu.ma>
2025-07-15 19:12:07 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div className="welcome-text">
-      Welcome to Marjane Groupe V3
+      Welcome to Marjane Groupe V4
     </div>
   );
 };
